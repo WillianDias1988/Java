e2d796d461364ea895d4a4d6a7c37444c25a3eeb
Willian Dias <williandias01@gmail.com>
2024-11-15 11:54:43 -0300

    User update

--- Course/src/main/java/com/example/course/services/UserService.java
+++ Course/src/main/java/com/example/course/services/UserService.java
@@ -33,4 +33,16 @@ public class UserService {
 	public void Delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setFone(obj.getFone());
+	}
 }
